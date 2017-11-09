876f2009227e02f1d4f6a1476b925634ef251384
Gorjan Velkovski <gorjan@info.nl>
2017-11-09 17:02:13 +0100

    add welcome endpoint

--- server.js
+++ server.js
@@ -18,7 +18,24 @@ app.listen(app.get('port'), function() {
 
 //routes
 app.get('/api/welcome', function(req, res) {
-  var url_parts = url.parse(request.url, true);
+  var url_parts = url.parse(req.url, true);
+  var query = JSON.parse(url_parts.query);
+
+  const message = {
+    "messages": [
+      {
+        "text": "hello from the bot's BE application!!"
+      },
+      {
+        "text": query
+      }
+    ]
+  };
+  res.status(200).json(message);
+});
+
+app.post('/api/welcome', function(req, res) {
+  var url_parts = url.parse(req.url, true);
   var query = JSON.parse(url_parts.query);
 
   const message = {
