b0b31036e39490fd8ff2feb091012d9241b47580
Gorjan Velkovski <gorjan@info.nl>
2017-11-10 14:20:05 +0100

    asdf

--- server.js
+++ server.js
@@ -66,15 +66,17 @@ app.get('/api/sandwiches/cheese', function(req, res) {
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
 
-  var newUsers = users.map(function(user) {
-    if (user.hasOwnProperty("username")) {
-      if (user.username === query["messenger user id"]) {
-        user["cheese"] = query["typeofcheese"];
-      }
-    }
-  });
+  // var newUsers = users.map(function(user) {
+  //   if (user.hasOwnProperty("username")) {
+  //     if (user.username === query["messenger user id"]) {
+  //       user["cheese"] = user["cheese"].push(query["typeofcheese"]);
+  //     }
+  //   }
+  // });
+  //
+  // users = newUsers;
 
-  users = newUsers;
+
 
   const message = {
     "redirect_to_blocks": ["What did you have on your sandwich?"],
